b03f845bd8a7e92020258f2c0e436354f9c10994
rachyharkov <44340813+rachyharkov@users.noreply.github.com>
2021-03-20 12:25:33 +0700

    Fixing Quest and Answer List also Revamp UI and Filling Quest Logic
    - Memperbaiki pengisian soal yang tadinya ada button next dan prev, sekarang bisa memilih jawaban langsung  lalu pergi ke quest selanjutnya
    - Mengatur lebar tabel soal agar mudah dilihat
    - [BUG] Saat opsi A dipilih tidak menunjukan indikator bahwa A dipilih, B C D bisa muncul indikatornyaa
    - Timer, Menunjukan Hasil Test (Meliputi jumlah soal yang dijawab, soal yang salah, total skor + nilai) masih on progress

--- application/views/peserta/confirmation_page.php
+++ application/views/peserta/confirmation_page.php
@@ -11,7 +11,7 @@
 						$arrayNumber = range(1, 900);
 						$baris = $arrayNumber;
 						?>
-						<table class="table table-bordered">
+						<table class="table table-bordered" style="width: 34vh;margin: auto;">
 							<tr>
 								<td>A</td>
 								<td>B</td>
@@ -46,42 +46,54 @@
 									<?php
 									$jlb = 1;
 
+
 									$var1 = '';
+									$var2 = '';
+									$var3 = '';
+									$var4 = '';
+									$var5 = '';
+									$var6 = '';
+									$var7 = '';
+									$var8 = '';
+									$var9 = '';
+									$var10 = '';
 
 									foreach ($datajawabanlistkolom1a as $k1) {
 										$var1 = $k1->listjawaban;
 									}
-									foreach ($datajawabanlistkolom1a as $k1) {
-										$var1 = $k1->listjawaban;
+									foreach ($datajawabanlistkolom2a as $k2) {
+										$var2 = $k2->listjawaban;
 									}
-									foreach ($datajawabanlistkolom1a as $k1) {
-										$var1 = $k1->listjawaban;
+									foreach ($datajawabanlistkolom3a as $k3) {
+										$var3 = $k3->listjawaban;
 									}
-									foreach ($datajawabanlistkolom1a as $k1) {
-										$var1 = $k1->listjawaban;
+									foreach ($datajawabanlistkolom4a as $k4) {
+										$var4 = $k4->listjawaban;
 									}
-									foreach ($datajawabanlistkolom1a as $k1) {
-										$var1 = $k1->listjawaban;
+									foreach ($datajawabanlistkolom5a as $k5) {
+										$var5 = $k5->listjawaban;
 									}
-									foreach ($datajawabanlistkolom1a as $k1) {
-										$var1 = $k1->listjawaban;
+									foreach ($datajawabanlistkolom6a as $k6) {
+										$var6 = $k6->listjawaban;
 									}
-									foreach ($datajawabanlistkolom1a as $k1) {
-										$var1 = $k1->listjawaban;
+									foreach ($datajawabanlistkolom7a as $k7) {
+										$var7 = $k7->listjawaban;
 									}
-									foreach ($datajawabanlistkolom1a as $k1) {
-										$var1 = $k1->listjawaban;
+									foreach ($datajawabanlistkolom8a as $k8) {
+										$var8 = $k8->listjawaban;
 									}
-									foreach ($datajawabanlistkolom1a as $k1) {
-										$var1 = $k1->listjawaban;
+									foreach ($datajawabanlistkolom9a as $k9) {
+										$var9 = $k9->listjawaban;
 									}
-									echo $var1;
+									foreach ($datajawabanlistkolom10a as $k10) {
+										$var10 = $k10->listjawaban;
+									}
+									$u = $var1.'-'.$var2.'-'.$var3.'-'.$var4.'-'.$var5.'-'.$var6.'-'.$var7.'-'.$var8.'-'.$var9.'-'.$var10;
+									$owo = array_map('trim',explode('-', $u));
 
-									foreach ($jawabanlist as $jl) {
-										$jlarray1 = explode('-', $jl->listjawaban);
+									foreach ($owo as $jl) {
+										$l = explode(' ', $jl);
 
-										foreach ($jlarray1 as $kjla1) {
-											$l = explode(' ', $kjla1);
 
 									 ?>
 									<tr class="barisnya<?php echo $jlb ?> barisss" hidden>
@@ -89,32 +101,26 @@
 											<div class="radioform">
 											  <div class="radiogroup">
 											    <input type="radio" id="option-onekolom<?php echo'baris'.$jlb ?>" name="selectorkolom<?php echo'baris'.$jlb ?>">
-											    <label for="option-one<?php echo'baris'.$jlb ?>"><?php echo $l[0] ?></label>
+											    <label onclick="setTimeout(function(){nextquest()},500);" for="option-one<?php echo'baris'.$jlb ?>"><?php echo $l[0] ?></label>
 											    <input type="radio" id="option-two<?php echo'baris'.$jlb ?>" name="selector<?php echo'baris'.$jlb ?>">
-											    <label for="option-two<?php echo'baris'.$jlb ?>"><?php echo $l[1] ?></label>
+											    <label onclick="setTimeout(function(){nextquest()},500);" for="option-two<?php echo'baris'.$jlb ?>"><?php echo $l[1] ?></label>
 											    <input type="radio" id="option-three<?php echo'baris'.$jlb ?>" name="selector<?php echo'baris'.$jlb ?>">
-											    <label for="option-three<?php echo'baris'.$jlb ?>"><?php echo $l[2] ?></label>
+											    <label onclick="setTimeout(function(){nextquest()},500);" for="option-three<?php echo'baris'.$jlb ?>"><?php echo $l[2] ?></label>
 											    <input type="radio" id="option-four<?php echo'baris'.$jlb ?>" name="selector<?php echo'baris'.$jlb ?>">
-											    <label for="option-four<?php echo'baris'.$jlb ?>"><?php echo $l[3] ?></label>
-											    <input type="radio" id="option-five<?php echo'baris'.$jlb ?>" name="selector<?php echo'baris'.$jlb ?>">
-											    <label for="option-five<?php echo'baris'.$jlb ?>"><?php echo $l[4] ?></label>
+											    <label onclick="setTimeout(function(){nextquest()},500);" for="option-four<?php echo'baris'.$jlb ?>"><?php echo $l[3] ?></label>
 											  </div>
 											</div>
 										</td>
 									</tr>
 								<?php 
 								$jlb++;
-								}
-								break;
 							} ?>
 								</table>
 							</div>
 						</div>
 					</div>
 					<div class="card-footer" id="footercard">
-						<button class="btn btn-primary btn-md" id="tombolnya" style="float: right; display: none;">Mulai Test</button>
-						<button class="btn btn-primary btn-md" id="nextbutton" type="button" style="float: right;">Selanjutnya</button>
-						<button class="btn btn-primary btn-md" id="prevbutton" type="button" style="float: right;">Sebelumnya</button>
+						<button class="btn btn-primary btn-md" id="startbuttontest" type="button" style="float: right;">Mulai Test</button>
 					</div>
 				</div>
 			</form>
--- application/views/peserta/footer.php
+++ application/views/peserta/footer.php
@@ -5,240 +5,142 @@
 
 <script type="text/javascript">
 	
+	function range(awal, akhir) {
+	  	return Array(akhir - awal + 1).fill().map((_, idx) => awal + idx);
+	}
+
+	var barisny = range(1, 900); // array sampe 900 (untuk baris jawaban list)
+	var index = -1;
+	var counter = $(this).index();
+	var max = $(barisny).length;
+
+	var indexsoal = 0;
+
+	var columnsoalcheckpoint = Array.from({length:31}, (v, i) => i * 30); //mulai dari 0, untuk index naekin 1
 	$(document).ready(function(){
 		
-		function range(awal, akhir) {
-		  	return Array(akhir - awal + 1).fill().map((_, idx) => awal + idx);
-		}
 
 
 
-		var barisny = range(1, 900); // array sampe 900 (untuk baris jawaban list)
-		var index = -1;
-		var counter = $(this).index();
-		var max = $(barisny).length;
 
-		var indexsoal = 0;
-
-		var columnsoalcheckpoint = Array.from({length:31}, (v, i) => i * 30 + 1); //mulai dari 0, untuk index naekin 1
-
-
-		$('#nextbutton').click(function(){
+		$('#startbuttontest').click(function(){
 			$('.soaltest').attr('hidden','true');
 			$('.barisss').attr('hidden','true');
-			if (index <= max) {
-				index++;
-				$('.barisnya' + barisny[index]).removeAttr('hidden');
-				$('#judulcard').text('Soal '+barisny[index]);
-				console.log(index);
-			}else{
-				alert("mentok kekanan")
-				$('.barisnya' + barisny[index]).removeAttr('hidden');
-				$('#judulcard').text('Soal '+barisny[index]);
-				console.log(index);
-			}
+			index++;
+			$(this).css('display','none');
+			$('.barisnya' + barisny[index]).removeAttr('hidden');
+			$('#judulcard').text('Soal '+barisny[index]);
+			$('.soalke1').removeAttr('hidden');
+		});
 
-			if (index < columnsoalcheckpoint[1]) {
-				$('#jenissoalnya').text('Soal Angka');
-				$('.soalke1').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[2]) {
-				$('#jenissoalnya').text('Soal Angka');
-				$('.soalke2').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[3]) {
-				$('#jenissoalnya').text('Soal Angka');
-				$('.soalke3').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[4]) {
-				$('#jenissoalnya').text('Soal Angka');
-				$('.soalke4').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[5]) {
-				$('#jenissoalnya').text('Soal Angka');
-				$('.soalke5').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[6]) {
-				$('#jenissoalnya').text('Soal Angka');
-				$('.soalke6').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[7]) {
-				$('#jenissoalnya').text('Soal Angka');
-				$('.soalke7').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[8]) {
-				$('#jenissoalnya').text('Soal Angka');
-				$('.soalke8').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[9]) {
-				$('#jenissoalnya').text('Soal Angka');
-				$('.soalke9').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[10]) {
-				$('#jenissoalnya').text('Soal Angka');
-				$('.soalke10').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[11]) {
-				$('#jenissoalnya').text('Soal Huruf');
-				$('.soalke11').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[12]) {
-				$('#jenissoalnya').text('Soal Huruf');
-				$('.soalke12').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[13]) {
-				$('#jenissoalnya').text('Soal Huruf');
-				$('.soalke13').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[14]) {
-				$('#jenissoalnya').text('Soal Huruf');
-				$('.soalke14').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[15]) {
-				$('#jenissoalnya').text('Soal Huruf');
-				$('.soalke15').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[16]) {
-				$('#jenissoalnya').text('Soal Huruf');
-				$('.soalke16').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[17]) {
-				$('#jenissoalnya').text('Soal Huruf');
-				$('.soalke17').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[18]) {
-				$('#jenissoalnya').text('Soal Huruf');
-				$('.soalke18').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[19]) {
-				$('#jenissoalnya').text('Soal Huruf');
-				$('.soalke19').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[20]) {
-				$('#jenissoalnya').text('Soal Huruf');
-				$('.soalke20').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[21]) {
-				$('#jenissoalnya').text('Soal Simbol');
-				$('.soalke21').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[22]) {
-				$('#jenissoalnya').text('Soal Simbol');
-				$('.soalke22').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[23]) {
-				$('#jenissoalnya').text('Soal Simbol');
-				$('.soalke23').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[24]) {
-				$('#jenissoalnya').text('Soal Simbol');
-				$('.soalke24').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[25]) {
-				$('#jenissoalnya').text('Soal Simbol');
-				$('.soalke25').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[26]) {
-				$('#jenissoalnya').text('Soal Simbol');
-				$('.soalke26').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[27]) {
-				$('#jenissoalnya').text('Soal Simbol');
-				$('.soalke27').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[28]) {
-				$('#jenissoalnya').text('Soal Simbol');
-				$('.soalke28').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[29]) {
-				$('#jenissoalnya').text('Soal Simbol');
-				$('.soalke29').removeAttr('hidden');
-			} else {
-				$('#jenissoalnya').text('Soal Simbol');
-				$('.soalke30').removeAttr('hidden');
-			}
-		})
 
-		$('#prevbutton').click(function(){
-			$('.barisss').attr('hidden','true');
-			if (index >= 1) {
-				index--;
-				$('.barisnya' + barisny[index]).removeAttr('hidden');
-				$('#judulcard').text('Soal '+barisny[index]);
-				console.log(index);
-			} else {
-				alert("mentok kekiri");
-				$('.barisnya' + barisny[index]).removeAttr('hidden');
-				$('#judulcard').text('Soal '+barisny[index]);
-				console.log(index);
-			}
-			
-			if (index < columnsoalcheckpoint[1]) {
-				$('#jenissoalnya').text('Soal Angka');
-				$('.soalke1').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[2]) {
-				$('#jenissoalnya').text('Soal Angka');
-				$('.soalke2').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[3]) {
-				$('#jenissoalnya').text('Soal Angka');
-				$('.soalke3').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[4]) {
-				$('#jenissoalnya').text('Soal Angka');
-				$('.soalke4').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[5]) {
-				$('#jenissoalnya').text('Soal Angka');
-				$('.soalke5').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[6]) {
-				$('#jenissoalnya').text('Soal Angka');
-				$('.soalke6').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[7]) {
-				$('#jenissoalnya').text('Soal Angka');
-				$('.soalke7').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[8]) {
-				$('#jenissoalnya').text('Soal Angka');
-				$('.soalke8').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[9]) {
-				$('#jenissoalnya').text('Soal Angka');
-				$('.soalke9').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[10]) {
-				$('#jenissoalnya').text('Soal Angka');
-				$('.soalke10').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[11]) {
-				$('#jenissoalnya').text('Soal Huruf');
-				$('.soalke11').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[12]) {
-				$('#jenissoalnya').text('Soal Huruf');
-				$('.soalke12').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[13]) {
-				$('#jenissoalnya').text('Soal Huruf');
-				$('.soalke13').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[14]) {
-				$('#jenissoalnya').text('Soal Huruf');
-				$('.soalke14').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[15]) {
-				$('#jenissoalnya').text('Soal Huruf');
-				$('.soalke15').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[16]) {
-				$('#jenissoalnya').text('Soal Huruf');
-				$('.soalke16').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[17]) {
-				$('#jenissoalnya').text('Soal Huruf');
-				$('.soalke17').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[18]) {
-				$('#jenissoalnya').text('Soal Huruf');
-				$('.soalke18').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[19]) {
-				$('#jenissoalnya').text('Soal Huruf');
-				$('.soalke19').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[20]) {
-				$('#jenissoalnya').text('Soal Huruf');
-				$('.soalke20').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[21]) {
-				$('#jenissoalnya').text('Soal Simbol');
-				$('.soalke21').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[22]) {
-				$('#jenissoalnya').text('Soal Simbol');
-				$('.soalke22').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[23]) {
-				$('#jenissoalnya').text('Soal Simbol');
-				$('.soalke23').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[24]) {
-				$('#jenissoalnya').text('Soal Simbol');
-				$('.soalke24').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[25]) {
-				$('#jenissoalnya').text('Soal Simbol');
-				$('.soalke25').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[26]) {
-				$('#jenissoalnya').text('Soal Simbol');
-				$('.soalke26').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[27]) {
-				$('#jenissoalnya').text('Soal Simbol');
-				$('.soalke27').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[28]) {
-				$('#jenissoalnya').text('Soal Simbol');
-				$('.soalke28').removeAttr('hidden');
-			} else if (index < columnsoalcheckpoint[29]) {
-				$('#jenissoalnya').text('Soal Simbol');
-				$('.soalke29').removeAttr('hidden');
-			} else {
-				$('#jenissoalnya').text('Soal Simbol');
-				$('.soalke30').removeAttr('hidden');
-			}
+	function nextquest(){
+		$('.soaltest').attr('hidden','true');
+		$('.barisss').attr('hidden','true');
+		if (index <= max) {
+			index++;
+			$('.barisnya' + barisny[index]).removeAttr('hidden');
+			$('#judulcard').text('Soal '+barisny[index]);
+			console.log(index);
+		}else{
+			alert("mentok kekanan")
+			$('.barisnya' + barisny[index]).removeAttr('hidden');
+			$('#judulcard').text('Soal '+barisny[index]);
+			console.log(index);
+		}
 
-		})	
-	});
+		if (index < columnsoalcheckpoint[1]) {
+			$('#jenissoalnya').text('Soal Angka');
+			$('.soalke1').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[2]) {
+			$('#jenissoalnya').text('Soal Angka');
+			$('.soalke2').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[3]) {
+			$('#jenissoalnya').text('Soal Angka');
+			$('.soalke3').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[4]) {
+			$('#jenissoalnya').text('Soal Angka');
+			$('.soalke4').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[5]) {
+			$('#jenissoalnya').text('Soal Angka');
+			$('.soalke5').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[6]) {
+			$('#jenissoalnya').text('Soal Angka');
+			$('.soalke6').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[7]) {
+			$('#jenissoalnya').text('Soal Angka');
+			$('.soalke7').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[8]) {
+			$('#jenissoalnya').text('Soal Angka');
+			$('.soalke8').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[9]) {
+			$('#jenissoalnya').text('Soal Angka');
+			$('.soalke9').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[10]) {
+			$('#jenissoalnya').text('Soal Angka');
+			$('.soalke10').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[11]) {
+			$('#jenissoalnya').text('Soal Huruf');
+			$('.soalke11').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[12]) {
+			$('#jenissoalnya').text('Soal Huruf');
+			$('.soalke12').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[13]) {
+			$('#jenissoalnya').text('Soal Huruf');
+			$('.soalke13').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[14]) {
+			$('#jenissoalnya').text('Soal Huruf');
+			$('.soalke14').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[15]) {
+			$('#jenissoalnya').text('Soal Huruf');
+			$('.soalke15').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[16]) {
+			$('#jenissoalnya').text('Soal Huruf');
+			$('.soalke16').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[17]) {
+			$('#jenissoalnya').text('Soal Huruf');
+			$('.soalke17').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[18]) {
+			$('#jenissoalnya').text('Soal Huruf');
+			$('.soalke18').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[19]) {
+			$('#jenissoalnya').text('Soal Huruf');
+			$('.soalke19').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[20]) {
+			$('#jenissoalnya').text('Soal Huruf');
+			$('.soalke20').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[21]) {
+			$('#jenissoalnya').text('Soal Simbol');
+			$('.soalke21').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[22]) {
+			$('#jenissoalnya').text('Soal Simbol');
+			$('.soalke22').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[23]) {
+			$('#jenissoalnya').text('Soal Simbol');
+			$('.soalke23').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[24]) {
+			$('#jenissoalnya').text('Soal Simbol');
+			$('.soalke24').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[25]) {
+			$('#jenissoalnya').text('Soal Simbol');
+			$('.soalke25').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[26]) {
+			$('#jenissoalnya').text('Soal Simbol');
+			$('.soalke26').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[27]) {
+			$('#jenissoalnya').text('Soal Simbol');
+			$('.soalke27').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[28]) {
+			$('#jenissoalnya').text('Soal Simbol');
+			$('.soalke28').removeAttr('hidden');
+		} else if (index < columnsoalcheckpoint[29]) {
+			$('#jenissoalnya').text('Soal Simbol');
+			$('.soalke29').removeAttr('hidden');
+		} else {
+			$('#jenissoalnya').text('Soal Simbol');
+			$('.soalke30').removeAttr('hidden');
+		}
+	}
 	    
 
 </script>
